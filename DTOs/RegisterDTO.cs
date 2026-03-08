1b450542824650c11ea41ed7cde7d90a328ace2b
Anshika-1030 <121483874+Anshika-1030@users.noreply.github.com>
2026-03-08 13:01:13 +0530

    Create RegisterDTO.cs
    RegisterDTO.cs

---
+++ DTOs/RegisterDTO.cs
@@ -0,0 +1,10 @@
+namespace SecureRateLimitAPI.DTOs;
+
+public class RegisterDTO
+{
+public string Username { get; set; }
+
+public string Password { get; set; }
+
+public string Role { get; set; }
+}
