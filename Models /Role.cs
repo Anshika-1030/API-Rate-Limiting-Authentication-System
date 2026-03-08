ff55f736385d2a894b465e9f8a1db2650192cb78
Anshika-1030 <121483874+Anshika-1030@users.noreply.github.com>
2026-03-08 13:15:22 +0530

    Create Role.cs
    Role.cs

---
+++ Models /Role.cs
@@ -0,0 +1,11 @@
+namespace SecureRateLimitAPI.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<User> Users { get; set; }
+    }
+}
